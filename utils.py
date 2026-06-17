3d5fe71b31d5f64d726c7fd382088c13df6d100d
thanEay <157990089+thanEay@users.noreply.github.com>
2026-06-17 09:41:02 -0400

    Implement ask_for_letter function and update ask_mode docstring

--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
 import random
 from random_words import hangman_words
 
-def ask_mode(input: str) -> str:
-    """Return a variable to determine manual or random word choice.
+def ask_mode(input: str) -> str: 
+    # TODO: Change all 'input' variables to other name to prevent overwriting build-in
+    # input function
+    """Return a variable to determine manual or random word choice. 
     
     Args: 
         input: The input by the user.
@@ -40,9 +42,37 @@ def check_letter(word: str, letter: str) -> int:
     if isinstance(letter, str) and len(letter) == 1:
         location = word.find(letter)
         return location # If letter is not in the word, will result in index of -1
-    else: 
+    else:
         raise ValueError(f"Invalid input: {letter}. Expected string with length of 1.")
 
 def choose_random_word() -> str:
     """Returns a random word (all lower) from random_words.py file."""
     return random.choice(hangman_words)
+
+def ask_for_letter(word: str, letter: str, counter: int, already_guessed: list):
+    """Letter checking and incorrect answer counter logic. 
+    
+    Args:
+        word: Secret word being guessed.
+        letter: Letter to search the word for.
+        counter: The number of incorrect answers this game, results in hangman sprite change.
+        already_guessed: A list of characters that were already guessed. Prevents the
+        player from increasing the counter on a letter that was already guessed. 
+    
+    Returns:
+        TODO
+    
+    Raises: 
+        TODO
+    """
+    try:
+        if check_letter(word, letter) != -1:
+            already_guessed.append(letter)
+            message = f"{letter} is in the word at index {check_letter(word, letter)}."
+            return check_letter(word, letter), message
+        else:
+            message = f"'{letter}' is NOT in the word."
+            return check_letter(word, letter), message
+    except ValueError:
+        print("Enter a single letter. No numbers or special characters are allowed.")
+
