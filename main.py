c2d582de4bbc78fb3f24fca746360d84f9c164e3
thanEay <157990089+thanEay@users.noreply.github.com>
2026-06-17 08:21:07 -0400

    Add ask mode logic

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from random import choice, randint
+from utils import *
 
-mode = input("Would you like to set your own word or randomly generate one?")
+mode_input = input("Would you like to set your own word or randomly generate one?\n")
+if ask_mode(mode_input) == "manual":
+    mode = "manual"
+elif ask_mode(mode_input) == "random":
+    mode = "random"
+else: 
+    print("Please enter an appropriate mode: 'manual' or 'random'.")
