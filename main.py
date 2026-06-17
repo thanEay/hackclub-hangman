fd425e871142364534ff08753a8721f9d47d3f8b
thanEay <157990089+thanEay@users.noreply.github.com>
2026-06-16 22:37:28 -0400

    Add test_functions.py to gitignore

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 from random import choice, randint
 
+mode = input("Would you like to set your own word or randomly generate one?")
--- test_functions.py
+++ test_functions.py
@@ -1,2 +1,6 @@
 from utils import *
 
+input = "r"
+# appropriate_values = ['value1', 'value2', 'value3']
+
+print(ask_mode(input))
