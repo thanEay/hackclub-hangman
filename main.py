f9aea9b201c9d78ffd3e5acabbe12e6683735a62
thanEay <157990089+thanEay@users.noreply.github.com>
2026-06-17 21:48:56 -0400

    Improved error handling for mode selection by using try-except block

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ from random import choice, randint
 from utils import *
 
 mode_input = input("Would you like to set your own word or randomly generate one?\n")
-if ask_mode(mode_input) == "manual":
-    mode = "manual"
-elif ask_mode(mode_input) == "random":
-    mode = "random"
-else: 
-    print("Please enter an appropriate mode: 'manual' or 'random'.")
+try:
+    if ask_mode(mode_input) == "manual":
+        mode = "manual"
+    elif ask_mode(mode_input) == "random":
+        mode = "random"
+    else: 
+        print("Please enter an appropriate mode: 'manual' or 'random'.")
+        exit()
+except ValueError:
+    print(f"'{mode_input}' is not an appropriate mode. Try 'm' for manual word input "\
+        "or 'r' for random word input.")
     exit()
 
 counter = 0
